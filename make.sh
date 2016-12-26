3778b44a500c0e3940cb8560f81d9ae52cd5716d
alexh-name <1@alexh.name>
2016-12-26 23:29:06 +0100

    include files to build site

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SHELLPATH="/bin"
+
+# ./scripts/gen-layout.sh
+SHELL="${SHELLPATH}/ksh" make
+./scripts/insert-files.sh
+# ./scripts/gen-thispage.sh
+./scripts/gen-gitlink.sh
+# ./scripts/gen-css.sh
