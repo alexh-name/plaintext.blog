479e53031203d1fdd29e189fa3ef14db160b17cf
alexh-name <1@alexh.name>
2017-01-08 21:59:19 +0100

    tree: set env to use nice utf8 chracters

--- scripts/tree.sh
+++ scripts/tree.sh
@@ -2,11 +2,13 @@
 
 TREE="$(
   printf "%s\n" '<p class="tree">'
-  /home/zckr/.linuxbrew/bin/tree -I 'google73ff9617eecd7684.html|*.css' -H . dest/ \
+  LC_ALL=en_US.UTF-8 /home/zckr/.linuxbrew/bin/tree \
+    -I 'google73ff9617eecd7684.html|*.css' -H . dest/ \
   | awk 'f;/<h1>Directory Tree/{f=1};/<br><br>/{exit}' \
   | sed 's/\&/\\\\\&/g'
   printf "%s\n" '</p>'
 )"
 
-awk -v tree="${TREE}" '/<p>TREE<\/p>/{sub(/<p>TREE<\/p>/,tree)};{print}' dest/index.html > dest/index.html.tmp
+awk -v tree="${TREE}" '/<p>TREE<\/p>/{sub(/<p>TREE<\/p>/,tree)};{print}' \
+  dest/index.html > dest/index.html.tmp
 mv dest/index.html.tmp dest/index.html
